544a4a55ad9337225cfc43b4d0ece0926102493c
poshjatel <86824168+poshjatel@users.noreply.github.com>
2024-11-12 00:34:40 -0800

    changed

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ fastify.register(async (fastify) => {
         let markQueue = [];
         let responseStartTimestampTwilio = null;
 
-        const openAiWs = new WebSocket('wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview-2024-10-01', {
+        const openAiWs = new WebSocket('wss://api.openai.com/v1/realtime?model=gpt-4o-realtime-preview', {
             headers: {
                 Authorization: `Bearer ${OPENAI_API_KEY}`,
                 "OpenAI-Beta": "realtime=v1"
